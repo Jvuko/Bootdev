9327357f377dd52c31947411d8f26b7a3135e8d2
Jvuko <jvuko147@gmail.com>
2025-11-27 20:25:23 -0600

    added command line arguments

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from stats import *
+import sys
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    path = "/home/vukoj/BootDev/Bootdev/books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
     text = get_book_text(path)
     words = count_words(text)
     char = count_chars(text)
     sorted = my_sort(char)
 
     print("============ BOOKBOT ============"
-          "Analyzing book found at books/frankenstein.txt..."
+          f"Analyzing book found at {sys.argv[1]}"
           "----------- Word Count ----------"
           f" {words} total words"
           "--------- Character Count -------")
